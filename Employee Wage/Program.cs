b38a73c5fb95008838513be4e5485639ebffbf9e
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-14 17:39:47 +0530

    [ShubhamPV] Adding Welcome

---
+++ Employee Wage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage
+{
+    class Welcome
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage");
+            Console.ReadLine();
+            
+        }
+
+
+    }
+}
