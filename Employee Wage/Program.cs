15298c30e3ec2e8876b358e99240730aebaa3aea
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-14 17:46:54 +0530

    Adding UC1Attendence

--- Employee Wage/Program.cs
+++ Employee Wage/Program.cs
@@ -6,16 +6,28 @@ using System.Threading.Tasks;
 
 namespace Employee_Wage
 {
-    class Welcome
+    class UC1
     {
-
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Employee Wage");
+            int IS_FULL_TIME = 1;
+            int empCheck;
+            Random random = new Random();
+            empCheck = random.Next(0, 2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
             Console.ReadLine();
-            
         }
 
 
     }
 }
+
+    
