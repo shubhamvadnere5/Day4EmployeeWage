8be9231d2b96c279e9f600940830aece398c0355
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-15 08:14:52 +0530

    Adding UC2 Employeewage

--- Employee Wage/Program.cs
+++ Employee Wage/Program.cs
@@ -6,23 +6,31 @@ using System.Threading.Tasks;
 
 namespace Employee_Wage
 {
-    class UC1
+    //UC2
+    class Salary
     {
         public static void Main(string[] args)
         {
             int IS_FULL_TIME = 1;
+            int empSalaryPerHr = 20;
+            int empPerHrDay = 8;
             int empCheck;
+            int salary;
             Random random = new Random();
             empCheck = random.Next(0, 2);
 
             if (empCheck == IS_FULL_TIME)
             {
                 Console.WriteLine("Present");
+                empPerHrDay = 8;
             }
             else
             {
                 Console.WriteLine("Absent");
+                empPerHrDay = 0;
             }
+            salary = empSalaryPerHr * empPerHrDay;
+            Console.WriteLine("Salary is:" + salary);
             Console.ReadLine();
         }
 
